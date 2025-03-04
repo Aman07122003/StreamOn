76c383bb15bba2740019961a1dc333a470116626
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-03-04 22:19:53 +0530

    DB connected

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import connectToDb from './db/dbConnect.js';
+import app from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectToDb()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on: ${process.env.PORT}`);
+    })
+})
+.catch((error) => console.log("MONGODB connection failed "))
